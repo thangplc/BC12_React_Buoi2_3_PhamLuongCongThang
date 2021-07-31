a3a4b95159b621eb9399bce1f10607859a844cd4
thangplc <thangplc1412@gmail.com>
2021-07-31 21:37:11 +0700

    complete project react choose car

--- react-ex-b2-b3/src/index.js
+++ react-ex-b2-b3/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/umd/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
